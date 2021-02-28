297ef0b6dc484735e55d40492d774fb0c7d4ce19
Jonathan Stoll <73194238+jonathanstoll0603@users.noreply.github.com>
2021-02-28 15:25:28 -0500

    fixed issues with fswritefile placing html in wrong location

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
-// const employee = require("./lib/Employee");
 const fs = require("fs")
 const inquirer = require("inquirer");
 const util = require("util"); //Util import for writeFileAsync
+const writeFileAsync = util.promisify(fs.writeFile); // For async function
 
+// All of the Classes to be used later
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 
-// const Employee = require("./lib/Employee");
-const writeFileAsync = util.promisify(fs.writeFile); // For async function
-// const generateHTML = require("./src/generateHTML");
-// const managerHTML = require("./src/managerHTML");
 
 // Function that initiates upon node being run
 async function init() {
+
     let teamProfileTemplate = "";
 
     // Global variables
     let employeeName;
     let id;
-    let role;
     let email;
     let officeNum;
     let github;
@@ -38,7 +35,7 @@ async function init() {
                 message: "Please enter the team manager's full name.",
                 name: 'employeeName',
                 validate: (answer) => {
-                    valid = /^[a-zA-Z]*$/.test(answer)
+                    valid = /^[a-zA-Z-,]+(\s{0,1}[a-zA-Z-, ])*$/.test(answer)
                     if (!valid) {
                         return console.log(" *Name input cannot contain any numbers or symbols. Try again.*")
                     }
@@ -93,35 +90,44 @@ async function init() {
 
             // Once finished, it creates a new Manager from the Manager class with responses as parameters
             const manager = new Manager(employeeName, id, email, officeNum);
-            const managerTemplate = fs.readFileSync("./src/manager.html");
 
-            // eval accesses the managerTemplate's local variables and fills them in with responses.
-            teamProfileTemplate = teamProfileTemplate + eval("`" + managerTemplate + "`")
+            // Places the manager responses inside of the manager.html file
+            employeeTemplate = fs.readFileSync("./src/manager.html");
+
+            // eval accesses the employeeTemplate's local variables and fills them in with responses.
+            teamProfileTemplate +="\n" + eval("`"+ employeeTemplate +"`");
+            console.log("Manager profile successfully created.")
+
+            // call the menu prompt to check if user wants to add more employees
+            return menuPrompt();
         }).catch(err => console.log(err));
         
     // Prompt for the menu that will appear after completing each team member prompt
-    await inquirer.prompt([
-        {
-            type: 'list',
-            message: 'Would you like to add an additional employee to your team profile?',
-            name: 'role',
-            choices: ["Engineer", "Intern", "Finished Building Team"]    
-        }
-    ]).then((menuAnswer) => {
-        console.log(menuAnswer);
-        
-        if (menuAnswer.role === "Engineer") {
-            console.log("Creating Engineer Profile...")
-            return addEngineerPrompt();
-        } else if (menuAnswer.role === "Intern") {
-            console.log("Creating Intern Profile...")
-            return addInternPrompt();
-        } else {
-            return;
-        }
-
-    }).catch(err => console.error(err));
+    async function menuPrompt() {
+        await inquirer.prompt([
+            {
+                type: 'list',
+                message: 'Would you like to add an additional employee to your team profile?',
+                name: 'role',
+                choices: ["Engineer", "Intern", "Finished Building Team"]    
+            }
+        ]).then((menuAnswer) => {
+            
+            if (menuAnswer.role === "Engineer") {
+                console.log("Creating Engineer Profile...")
+                // Returns the engineer prompt questions
+                return addEngineerPrompt();
+            } else if (menuAnswer.role === "Intern") {
+                console.log("Creating Intern Profile...")
+                // Returns the intern prompt questions
+                return addInternPrompt();
+            } else {
+                return;
+            }
     
+        }).catch(err => console.error(err));
+    }
+
     // // // Variable that hold the questions and answers prompt for Addtional Employees
     async function addEngineerPrompt() {
         await inquirer.prompt([
@@ -130,7 +136,7 @@ async function init() {
                 message: "What is the name of the employee you'd like to place in the team portal?",
                 name: 'employeeName',
                 validate: (answer) => {
-                    valid = /^[a-zA-Z]*$/.test(answer)
+                    valid = /^[a-zA-Z-,]+(\s{0,1}[a-zA-Z-, ])*$/.test(answer)
                     if (!valid) {
                         return console.log(" *Name input cannot contain any numbers or symbols. Try again.*")
                     }
@@ -175,11 +181,16 @@ async function init() {
             email = response.email;
             role = response.role;
             github = response.github;
-            // creates a new employee class with response answers.
-            let newEngineer = new Engineer(employeeName, id, email, github);
-            const engineerTemplate = fs.readFileSync("./src/intern.html");
 
-            teamProfileTemplate += eval("`" + engineerTemplate + "`")
+            // creates a new employee class with response answers.
+            let engineer = new Engineer(employeeName, id, email, github);
+
+            // Places the engineer responses inside of the engineer.html file
+            employeeTemplate = fs.readFileSync("./src/engineer.html");
+
+            teamProfileTemplate += "\n" + eval("`"+ employeeTemplate +"`")
+            console.log("Engineer profile successfully created.")
+
             return menuPrompt();
         })
     }
@@ -191,7 +202,7 @@ async function init() {
                 message: "What is the name of the employee you'd like to place in the team portal?",
                 name: 'employeeName',
                 validate: (answer) => {
-                    valid = /^[a-zA-Z]*$/.test(answer)
+                    valid = /^[a-zA-Z-,]+(\s{0,1}[a-zA-Z-, ])*$/.test(answer)
                     if (!valid) {
                         return console.log(" *Name input cannot contain any numbers or symbols. Try again.*")
                     }
@@ -239,34 +250,30 @@ async function init() {
             email = response.email;
             role = response.role;
             school = response.school;
-            // creates a new employee class with response answers.
-            let newIntern = new Intern(employeeName, id, email, school);
-            
-            // Once finished, it creates a new Employee for the manager w/ responses as parameters
-            const internTemplate = fs.readFileSync("./src/intern.html");
 
-            teamProfileTemplate += eval("`" + internTemplate + "`")
+            // creates a new Intern class with response answers.
+            let intern = new Intern(employeeName, id, email, school);
+
+            // Places the intern responses inside of the intern.html file
+            employeeTemplate = fs.readFileSync("./src/intern.html");
+
+            teamProfileTemplate += eval("`"+ employeeTemplate +"`")
+            console.log("Intern profile successfully created.")
 
             // Recall the menu prompt to see if user wants to add another team member
             return menuPrompt();
         }).catch(err => console.error(err));
     }
 
-    // Reads main.html and places html in a variable
+    // Reads generate.html and places the contents of the file in a variable
     const generateFinalPage = fs.readFileSync("./src/generate.html");
-            
-    // Use eval to implement template literals in main.html and places teamHTML inside main template
-    teamProfileTemplate += eval("`" + generateFinalPage + "`");
+                        
+    // takes all of the team profiles that were created, and places them inside of generateFinalPage variable.
+    teamProfileTemplate = eval("`"+ generateFinalPage +"`");
 
-    // write file to new team.html file
-    fs.writeFile("./dist/index.html", teamProfileTemplate, function(err) {
-
-        if (err) {
-        return console.log(err);
-        }
-        
-        console.log("Successfully Created Team Member Profile.");
-    })
+    // Writes all of the stored team profiles templates and their contents to a final html page.
+    writeFileAsync("./dist/index.html", teamProfileTemplate);
+    console.log("Team profile page successfully created!")
 }
 
 init();
